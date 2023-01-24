3c8056a0ae1238e7445a38006e130da326f40199
sandwools <arktis@foxmail.com>
2023-01-24 22:33:00 +0800

    修复CSS覆盖的问题：用ID指定

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -7,21 +7,38 @@
                 <el-carousel 
                 indicator-position="outside"
                 height="400px"
+                id="carID"
                 >
                     <div class="carouse-text">
                         asdsad
                     </div>
-                    <el-carousel-item v-for="item in 4" :key="item">
+
+                    <el-carousel-item v-for="item in 4" :key="item" >
                         <h3 class="small justify-center" text="2xl">{{ item }}</h3>
                     </el-carousel-item>
                 </el-carousel>
             </div>
 
             <div class="view-text">
-                <div v-for="index in 10" :key="index">
+                <!-- <div v-for="index in 10" :key="index">
                     <h3>{{index}}</h3>
                     <hr/>
-                    <img src="https://i.328888.xyz/2023/01/22/O5Qfo.jpeg"/>
+                    
+                </div> -->
+                <div class="infinite-list-wrapper" style="overflow: auto">
+                    <ul
+                        v-infinite-scroll="load"
+                        class="list"
+                        :infinite-scroll-disabled="disabled"
+                        >
+                        <div v-for="i in count" :key="i" class="list-item">
+                            <p>{{ i }}</p>
+                            <!-- z注意要使用图片懒加载 -->
+                            <img src="https://i.328888.xyz/2023/01/22/O5Qfo.jpeg"/>
+                        </div>
+                    </ul>
+                    <p v-if="loading">Loading...</p>
+                    <p v-if="noMore">No more</p>
                 </div>
             </div>
 
@@ -31,17 +48,39 @@
 
 <script lang="ts">
 import { Options, Vue} from 'vue-class-component';
-import Sidebar from '../components/MainSidebar.vue'
+import Sidebar from '../components/MainSidebar.vue';
+import {defineComponent,computed,ref} from 'vue'
 
-@Options({
+
+
+
+
+export default defineComponent({
     components: {
         Sidebar
     },
+    setup(){
+        const count = ref(10)
+        const loading = ref(false)
+        const noMore = computed(() => count.value >= 20)
+        const disabled = computed(() => loading.value || noMore.value)
+        const load = () => {
+            loading.value = true;
+            setTimeout(() => {
+                count.value += 2
+                loading.value = false
+                }, 1000)
+            }
+        return{
+            count,
+            loading,
+            disabled,
+            noMore,
+            load
+        }
+    }
 })
-
-export default class  MainView extends Vue{
-
-}
+    
 </script>
 
 <style scoped lang="less">
@@ -75,21 +114,20 @@ export default class  MainView extends Vue{
     }
 }
 
-
-
-
 </style>
 
 <style lang="less">
-@iwidth:20%;
-.el-carousel__indicators--outside{
-    background-color: #FFA156;
-    position: absolute;
-    width: @iwidth;
-    left:100%-@iwidth;
-    button{
-        height: 6px;
-        border-radius: 20%;
+@i-width:20%;
+#carID{
+    .el-carousel__indicators{
+        position: absolute;
+        background-color: #FFA156;
+        width: @i-width;
+        left:100%-@i-width;
+        button{
+            height: 6px;
+            border-radius: 20%;
+        }
     }
 }
 </style>
