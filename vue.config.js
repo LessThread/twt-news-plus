ff4767d4279a5b0b0bd9c438d42a5a37c0dc557a
LiuYuanchi <liu_yuanchi@outlook.com>
2023-09-24 16:51:39 +0800

    fix:修复大图留白的问题

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   //根目录
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title= '天外天新闻网'
+        return args
+      })
+  }
 };
 
   
